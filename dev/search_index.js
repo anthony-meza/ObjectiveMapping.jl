70e64310fa7f1f5896d5affa1def945dce96e13c
Documenter.jl <documenter@juliadocs.github.io>
2024-11-28 19:24:24 +0000

    build based on 0d64727

---
+++ dev/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"","page":"Home","title":"Home","text":"CurrentModule = ObjectiveMapping","category":"page"},{"location":"#ObjectiveMapping","page":"Home","title":"ObjectiveMapping","text":"","category":"section"},{"location":"","page":"Home","title":"Home","text":"Documentation for ObjectiveMapping.","category":"page"},{"location":"","page":"Home","title":"Home","text":"","category":"page"},{"location":"","page":"Home","title":"Home","text":"Modules = [ObjectiveMapping]","category":"page"}]
+}
